4349e8fef10a10521a1aa18d0f83e9e50f746552
Andrew C Kirby <akirby@uwyo.edu>
2024-02-09 12:35:00 -0500

    Add back occa.

---
+++ occa/include/occa/defines/occa.hpp
@@ -0,0 +1,22 @@
+#ifndef OCCA_DEFINES_OCCA_HEADER
+#define OCCA_DEFINES_OCCA_HEADER
+
+#include <occa/defines/compiledDefines.hpp>
+
+#define OCCA_MAJOR_VERSION 1
+#define OCCA_MINOR_VERSION 6
+#define OCCA_PATCH_VERSION 0
+#define OCCA_VERSION       10600
+#define OCCA_VERSION_STR   "1.6.0"
+
+#define OKL_MAJOR_VERSION 1
+#define OKL_MINOR_VERSION 6
+#define OKL_PATCH_VERSION 0
+#define OKL_VERSION       10600
+#define OKL_VERSION_STR   "1.6.0"
+
+#define OCCA_MAX_ARGS 60
+
+#define OCCA_DEFAULT_MEM_BYTE_ALIGN 32
+
+#endif
